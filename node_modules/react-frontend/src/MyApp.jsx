377722673e91515b39d49c61a5a83c2566c70532
Euclid <eperegri@calpoly.edu>
2025-01-15 20:23:55 -0800

    step 6 commit

---
+++ node_modules/react-frontend/src/MyApp.jsx
@@ -0,0 +1,24 @@
+// // src/MyApp.jsx
+// import React from "react";
+
+// function MyApp() {
+//   return (
+//     <div>
+//       <h1>Hello, React!</h1>
+//     </div>
+//   );
+// }
+// export default MyApp;
+// src/MyApp.jsx
+import React from "react";
+import Table from "./Table";
+
+function MyApp() {
+  return (
+    <div className="container">
+      <Table />
+    </div>
+  );
+}
+
+export default MyApp;
